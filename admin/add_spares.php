af38645ac8765cdfda8d338522c414033443099c
HarisPasival <624234018@parichat.skru.ac.th>
2022-09-25 00:29:26 +0700

    new 25/9/65

---
+++ admin/add_spares.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <script src="https://kit.fontawesome.com/79a0376aeb.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/form.css" />
+    <title>หน้าหลัก</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
+
+        * {
+            font-family: 'Kanit', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+            </button>
+            <a class="navbar-brand me-auto ms-lg-0 ms-3 fw-bold" href="#">Khawna Phasoob</a>
+        </div>
+    </nav>
+    <!-- navbar -->
+    <!-- sidebar -->
+    <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
+        <div class="offcanvas-body p-0">
+            <nav class="navbar-dark">
+                <ul class="navbar-nav">
+                    <li class="mt-3">
+                        <div class="text-muted small fw-bold px-3">
+                            แดชบอร์ด
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 mt-3 my-3">
+                            <span class="me-2"><i class="fa-solid fa-table-columns"></i></span>
+                            <span>หน้าหลัก</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3">
+                            ข้อมูลผู้ใช้ระบบ
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-user-tie"></i></span>
+                            <span>ข้อมูลพนักงาน</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-user"></i></span>
+                            <span>ข้อมูลลูกค้า</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            ข้อมูลพื้นฐาน
+                        </div>
+                    </li>
+                    <li>
+                        <a href="spares.php" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-car-rear"></i></span>
+                            <span>ข้อมูลอะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="model.php" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-car-side"></i></span>
+                            <span>ข้อมูลประเภทฝาสูบ</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            ข้อมูลการสั่งซื้อ
+                        </div>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 sidebar-link" data-bs-toggle="collapse" href="#layouts">
+                            <span class="me-2"><i class="fa-solid fa-cart-shopping"></i></span>
+                            <span>สั่งซื้ออะไหล่</span>
+                            <span class="ms-auto">
+                                <span class="right-icon">
+                                    <i class="fa-solid fa-sort-down"></i>
+                                </span>
+                            </span>
+                        </a>
+                        <div class="collapse" id="layouts">
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="#" class="nav-link px-3">
+                                        <span class="me-2"><i class="fa-solid fa-cart-plus"></i></span>
+                                        <span>รายการที่ทำการสั่งซื้อ</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link px-3">
+                                        <span class="me-2"><i class="fa-solid fa-cart-arrow-down"></i></span>
+                                        <span>รายการที่ทำการรับเข้าแล้ว</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            รายงาน
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-flag"></i></span>
+                            <span>รายงานการสั่งซื้ออะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-flag"></i></span>
+                            <span>รายงานการรับเข้าอะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-right-from-bracket" style="color: red;"></i></span>
+                            <span>ออกจากระบบ</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!-- sidebar -->
+    <!-- content -->
+    <main class="mt-5 pt-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">ข้อมูลพื้นฐาน</li>
+                        <li class="breadcrumb-item">ข้อมูลอะไหล่</li>
+                        <li class="breadcrumb-item active text-primary">เพิ่มข้อมูลอะไหล่</li>
+                    </ol>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                        <div class="card-header bg-dark">
+                            <span class="text-light">เพิ่มข้อมูลอะไหล่</span>
+                        </div>
+                        <div class="card-body">
+                        <form action="#" method="POST" class="row g-3">
+                            <div class="col-md-4 mb-3">
+                                <label>ชื่ออะไหล่:</label>
+                                <input type="text" name="spare_name" class="form-control" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <label>จำนวน:</label>
+                                <input type="text" name="spare_quanlity" class="form-control" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <label>ราคา:</label>
+                                <input type="text" name="spare_cost" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" name="save_spare" class="btn btn-primary">เพิ่มข้อมูล</button>
+                                <a href="spares.php" class="btn btn-danger">ย้อนกลับ</a>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- end content -->
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/jquery.dataTables.min.js"></script>
+    <script src="../js/dataTables.bootstrap5.min.js"></script>
+    <script src="../js/script.js"></script>
+</body>
+
+</html>
---
+++ admin/spares.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <script src="https://kit.fontawesome.com/79a0376aeb.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>ข้อมูลอะไหล่</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
+
+        * {
+            font-family: 'Kanit', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+            </button>
+            <a class="navbar-brand me-auto ms-lg-0 ms-3 fw-bold" href="#">Khawna Phasoob</a>
+        </div>
+    </nav>
+    <!-- navbar -->
+    <!-- sidebar -->
+    <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
+        <div class="offcanvas-body p-0">
+            <nav class="navbar-dark">
+                <ul class="navbar-nav">
+                    <li class="mt-3">
+                        <div class="text-muted small fw-bold px-3">
+                            แดชบอร์ด
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 mt-3 my-3">
+                            <span class="me-2"><i class="fa-solid fa-table-columns"></i></span>
+                            <span>หน้าหลัก</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3">
+                            ข้อมูลผู้ใช้ระบบ
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-user-tie"></i></span>
+                            <span>ข้อมูลพนักงาน</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-user"></i></span>
+                            <span>ข้อมูลลูกค้า</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            ข้อมูลพื้นฐาน
+                        </div>
+                    </li>
+                    <li>
+                        <a href="spares.php" class="nav-link active px-3">
+                            <span class="me-2"><i class="fa-solid fa-car-rear"></i></span>
+                            <span>ข้อมูลอะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="model.php" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-car-side"></i></span>
+                            <span>ข้อมูลประเภทฝาสูบ</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            ข้อมูลการสั่งซื้อ
+                        </div>
+                    </li>
+                    <li>
+                        <a class="nav-link px-3 sidebar-link" data-bs-toggle="collapse" href="#layouts">
+                            <span class="me-2"><i class="fa-solid fa-cart-shopping"></i></span>
+                            <span>สั่งซื้ออะไหล่</span>
+                            <span class="ms-auto">
+                                <span class="right-icon">
+                                    <i class="fa-solid fa-sort-down"></i>
+                                </span>
+                            </span>
+                        </a>
+                        <div class="collapse" id="layouts">
+                            <ul class="navbar-nav ps-3">
+                                <li>
+                                    <a href="#" class="nav-link px-3">
+                                        <span class="me-2"><i class="fa-solid fa-cart-plus"></i></span>
+                                        <span>รายการที่ทำการสั่งซื้อ</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link px-3">
+                                        <span class="me-2"><i class="fa-solid fa-cart-arrow-down"></i></span>
+                                        <span>รายการที่ทำการรับเข้าแล้ว</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold px-3 mb-3 my-3">
+                            รายงาน
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-flag"></i></span>
+                            <span>รายงานการสั่งซื้ออะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-flag"></i></span>
+                            <span>รายงานการรับเข้าอะไหล่</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3">
+                            <span class="me-2"><i class="fa-solid fa-right-from-bracket" style="color: red;"></i></span>
+                            <span>ออกจากระบบ</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!-- sidebar -->
+    <!-- content -->
+    <main class="mt-5 pt-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 mt-2">
+                    <h4>ข้อมูลอะไหล่</h4>
+                    <a href="add_spares.php" class="btn btn-success"><i class="fa-solid fa-circle-plus"></i> เพิ่มข้อมูล</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mb-3 mt-2">
+                    <div class="card">
+                        <div class="card-header">
+                            <span>ตารางข้อมูลอะไหล่</span>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example" class="table table-striped data-table" style="width: 100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Hermione Butler</td>
+                                            <td>Regional Director</td>
+                                            <td>London</td>
+                                            <td>47</td>
+                                            <td>2011/03/21</td>
+                                            <td>$356,250</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Lael Greer</td>
+                                            <td>Systems Administrator</td>
+                                            <td>London</td>
+                                            <td>21</td>
+                                            <td>2009/02/27</td>
+                                            <td>$103,500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jonas Alexander</td>
+                                            <td>Developer</td>
+                                            <td>San Francisco</td>
+                                            <td>30</td>
+                                            <td>2010/07/14</td>
+                                            <td>$86,500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Shad Decker</td>
+                                            <td>Regional Director</td>
+                                            <td>Edinburgh</td>
+                                            <td>51</td>
+                                            <td>2008/11/13</td>
+                                            <td>$183,000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Michael Bruce</td>
+                                            <td>Javascript Developer</td>
+                                            <td>Singapore</td>
+                                            <td>29</td>
+                                            <td>2011/06/27</td>
+                                            <td>$183,000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Donna Snider</td>
+                                            <td>Customer Support</td>
+                                            <td>New York</td>
+                                            <td>27</td>
+                                            <td>2011/01/25</td>
+                                            <td>$112,000</td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- end content -->
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/jquery.dataTables.min.js"></script>
+    <script src="../js/dataTables.bootstrap5.min.js"></script>
+    <script src="../js/script.js"></script>
+</body>
+
+</html>
